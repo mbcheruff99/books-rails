3e56db6ae9096fb9f90d32a755fb121b2809f0d8
bshin27 <125755440+bshin27@users.noreply.github.com>
2026-01-19 21:38:06 -0500

    Sessions controller with create and destroy actios, routes, tests. Includes cookies

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
 
   get "up" => "rails/health#show", as: :rails_health_check
 
+
+  # Users
   post "/users" => "users#create"
 
+  # Sessions
+  post "/sessions" => "sessions#create"
+  delete "/sessions" => "sessions#destroy"
+  
 end
